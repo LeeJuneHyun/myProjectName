e50f4b064fac27db93a4c9664d795ea6f13323c1
admin <admin@DESKTOP-2VQM46O>
2022-03-04 12:42:35 +0900

    2cc

--- src/com/human/ex/JavaTest04.java
+++ src/com/human/ex/JavaTest04.java
@@ -13,11 +13,29 @@ public class JavaTest04 {
 		System.out.println("키:" + a3);
 		System.out.println("\n");
 		
+		//문제 2
+		System.out.println("정수자료형 : int , byte , short , long");
+		System.out.println("실수자료형 : float , double");
+		System.out.println("문자자료형 : char");
+		System.out.println("논리형 : boolean");
+		System.out.println("참조자료형 : 문자열 String , 배열 , 클래스 , 열거형");
+		System.out.println("\n");
+		
+		//문제 3
+		System.out.println("10 : int 형");
+		System.out.println("1. : double 형");
+		System.out.println("6F : float 형");
+		System.out.println("23d : double 형");
+		System.out.println("14E10 : double 형");
+		System.out.println("\'a\' : char 형");
+		System.out.println("\n");
+		
 		//문제 4
 		int a = 5;
 		int b = 10;
 		
-		System.out.println((a+3)+b);
+		System.out.println( a + b );
+		System.out.println("\n");
 		
 		
 
