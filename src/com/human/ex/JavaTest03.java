417d88630b71a9869c0f05a1884733228a1208e3
admin <admin@DESKTOP-2VQM46O>
2022-03-04 12:21:47 +0900

    2cc

--- src/com/human/ex/JavaTest03.java
+++ src/com/human/ex/JavaTest03.java
@@ -64,5 +64,68 @@ public class JavaTest03 {
 		
 		System.out.println(Math.PI);
 		System.out.println("\n");
+		
+		//byte
+		byte a1 = 10;
+		byte a2;
+		a2 = 11;
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a1+a2);
+		System.out.println(a1+1);
+		System.out.println("\n");
+		//short
+		short a3 = 10;
+		short a4;
+		a4 =11;
+		System.out.println(a3);
+		System.out.println(a4);
+		System.out.println(a3+a4);
+		System.out.println(a3+1);
+		System.out.println("\n");
+		//int
+		int a5 = 10;
+		int a6;
+		a6 = 11;
+		System.out.println(a5);
+		System.out.println(a6);
+		System.out.println(a5+a6);
+		System.out.println(a5+1);
+		System.out.println("\n");
+		//long
+		long a7 = 10L;
+		long a8;
+		a8 = 11l;
+		System.out.println(a7);
+		System.out.println(a8);
+		System.out.println(a7+a8);
+		System.out.println(a7+1);
+		System.out.println("\n");
+		//float
+		float a9=10.23f;
+		float a10;
+		a10=11.f;
+		System.out.println(a9);
+		System.out.println(a10);
+		System.out.println(a9+a10);
+		System.out.println(a9+1.4f);
+		System.out.println("\n");
+		//double
+		double a11=10.23;
+		double a12;
+		a12=11.94;
+		System.out.println(a11);
+		System.out.println(a12);
+		System.out.println(a11+a12);
+		System.out.println(a11+10.4);
+		System.out.println("\n");
+		//String
+		String st1 = "Hello";
+		String st2 = "World";
+		System.out.println(st1);
+		System.out.println(st1+ "World");
+		System.out.println(st1 + st2);
+		String st3 = st1 + st2;
+		System.out.println(st3);
 	}
 }
